8d878f5bad2548c970bb458cf2b9a616e5ce1c66
rafin007 <rafin.ryan.07@outlook.com>
2018-12-25 06:02:30 +0600

    game logic updated

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-/*
-GAME RULES:
+var scores, roundScore, activePlayer;
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
 
-*/
+//initially set all score to 0
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+
+//hide the dice initially
+document.querySelector('.dice').style.display = "none";
+
+//dice roll button action
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    //generate random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //update the dice image
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.style.display = "block";
+
+    //update the round score only IF it is NOT 1
+    if (dice !== 1) {
+        //add the score
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }
+    else {
+        //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+
+        //toggle active player
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //hide the dice
+        document.querySelector('.dice').style.display = 'none';
+
+    }
+
+});
